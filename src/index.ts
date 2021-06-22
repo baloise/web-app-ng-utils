6ab4a17a9842542002f0daf03b9aea2a6d4eb07b
hirsch <gery.hirschfeld@w3tec.ch>
2021-06-22 16:51:12 +0200

    chore(): add angular setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 
+import { NgModule } from '@angular/core'
+import { BalTestService } from './service'
+
+@NgModule({
+  imports: [],
+  providers: [BalTestService],
+})
+export class BaloiseTestModule {}
---
+++ src/service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core'
+
+@Injectable()
+export class BalTestService {
+  say(): void {
+    console.log('Hello Lazar & Gery')
+  }
+}
