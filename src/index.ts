a73c5cc71f51b7e84b0827348e81a20be7fa0a40
hirsch <gery.hirschfeld@w3tec.ch>
2021-07-30 11:05:47 +0200

    fix(): export models

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./asset";
 export * from "./form";
 export * from "./pipes";
+export * from "./models";
---
+++ src/models/index.ts
@@ -0,0 +1,3 @@
+export * from "./gtm.model";
+export * from "./option.model";
+export * from "./tooltip.model";
